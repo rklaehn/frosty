ad696f407efddf16fbb884c923291ace2673c033
Ruediger Klaehn <rklaehn@protonmail.com>
2024-09-29 17:43:41 +0300

    implement co-signing protocol

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ use anyhow::Context;
 use clap::Parser;
 use frost_ed25519::{
     self as frost,
-    keys::{IdentifierList, SecretShare},
+    keys::{IdentifierList, PublicKeyPackage, SecretShare},
     Ed25519ScalarField, Field, Identifier,
 };
-use iroh_net::key::PublicKey;
+use iroh_net::{
+    discovery::{dns::DnsDiscovery, pkarr::PkarrPublisher},
+    endpoint::{RecvStream, SendStream},
+    key::{PublicKey, SecretKey},
+};
 use rand::thread_rng;
 use sha2::{Digest, Sha512};
 use std::{collections::BTreeMap, path::PathBuf, str::FromStr};
+use tokio::io::AsyncWriteExt;
+
+const COSIGN_ALPN: &[u8] = b"FROST_COSIGN";
 
 #[derive(Debug, clap::Parser)]
 struct Args {
@@ -19,6 +26,7 @@ struct Args {
 #[derive(Debug, clap::Parser)]
 enum Command {
     Split(SplitArgs),
+    SignLocal(SignLocalArgs),
     Sign(SignArgs),
     Cosign(CosignArgs),
 }
@@ -38,7 +46,7 @@ struct SplitArgs {
 }
 
 #[derive(Debug, clap::Parser)]
-struct SignArgs {
+struct SignLocalArgs {
     directories: Vec<String>,
     #[clap(long)]
     message: String,
@@ -47,8 +55,22 @@ struct SignArgs {
 }
 
 #[derive(Debug, clap::Parser)]
+struct SignArgs {
+    cosigners: Vec<PublicKey>,
+    #[clap(long)]
+    message: String,
+    #[clap(long)]
+    key: PublicKey,
+    /// Optional path to the directory where the fragments are stored
+    /// If not provided, the current directory is used
+    #[clap(long)]
+    data_path: Option<PathBuf>,
+}
+
+#[derive(Debug, clap::Parser)]
 struct CosignArgs {
     /// Optional path to the directory where the fragments are stored
+    /// If not provided, the current directory is used
     #[clap(long)]
     data_path: Option<PathBuf>,
 }
@@ -87,7 +109,7 @@ fn split(args: SplitArgs) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn sign(args: SignArgs) -> anyhow::Result<()> {
+fn sign_local(args: SignLocalArgs) -> anyhow::Result<()> {
     let mut parts = Vec::new();
     let mut paths = Vec::new();
     let key = args.key;
@@ -158,6 +180,9 @@ async fn handle_cosign_request(
     tracing::info!("Got fragment, creating commitment");
     let (nonces, commitments) =
         frost::round1::commit(key_package.signing_share(), &mut thread_rng());
+    let identifier_bytes = key_package.identifier().serialize();
+    tracing::info!("Sending identifier");
+    send.write_all(&identifier_bytes).await?;
     let commitments_bytes = commitments.serialize()?;
     let commitments_bytes_len = commitments_bytes.len() as u32;
     tracing::info!("Sending commitment");
@@ -175,12 +200,105 @@ async fn handle_cosign_request(
     let signature_share_bytes = signature_share.serialize();
     tracing::info!("Sending signature share");
     send.write_all(&signature_share_bytes).await?;
-    send.finish()?;
+    send.flush().await?;
+    tracing::info!("Finished handling cosign request");
+    futures::future::pending::<()>().await;
+    Ok(())
+}
+
+async fn send_cosign_request_round1(
+    endpoint: &iroh_net::Endpoint,
+    cosigner: &PublicKey,
+    key: &PublicKey,
+) -> anyhow::Result<(
+    SendStream,
+    RecvStream,
+    Identifier,
+    frost::round1::SigningCommitments,
+)> {
+    let connection = endpoint.connect((*cosigner).into(), COSIGN_ALPN).await?;
+    let (mut send, mut recv) = connection.open_bi().await?;
+    tracing::info!("Sending cosign request for key {} to {}", key, cosigner);
+    send.write_all(key.as_bytes()).await?;
+    let mut identifier_bytes = [0u8; 32];
+    recv.read_exact(&mut identifier_bytes).await?;
+    let identifier = Identifier::deserialize(&identifier_bytes)?;
+    let mut commitments_len_bytes = [0u8; 4];
+    recv.read_exact(&mut commitments_len_bytes).await?;
+    let commitments_len = u32::from_be_bytes(commitments_len_bytes) as usize;
+    let mut commitments_bytes = vec![0u8; commitments_len];
+    recv.read_exact(&mut commitments_bytes).await?;
+    let commitments = frost::round1::SigningCommitments::deserialize(&commitments_bytes)?;
+    tracing::info!("Received commitments");
+    Ok((send, recv, identifier, commitments))
+}
+
+async fn sign(args: SignArgs) -> anyhow::Result<()> {
+    let key = args.key;
+    let data_path = args.data_path.unwrap_or_else(|| PathBuf::from("."));
+    let secret_share_path = data_path.join(format!("{}.secret", key));
+    tracing::info!("Reading secret share from {}", secret_share_path.display());
+    let secret_share_bytes = std::fs::read(&secret_share_path)?;
+    let secret_share = SecretShare::deserialize(&secret_share_bytes)?;
+    let key_package = frost::keys::KeyPackage::try_from(secret_share)?;
+    let public_key_package_path = data_path.join(format!("{}.pub", key));
+    tracing::info!(
+        "Reading public key package from {}",
+        secret_share_path.display()
+    );
+    let public_key_package_bytes = std::fs::read(&public_key_package_path)?;
+    let public_key_package = PublicKeyPackage::deserialize(&public_key_package_bytes)?;
+    tracing::info!("Creating local commitment");
+    let (nonce, commitments) =
+        frost::round1::commit(key_package.signing_share(), &mut thread_rng());
+    tracing::info!("{} co-signers required", key_package.min_signers() - 1);
+    let discovery = DnsDiscovery::n0_dns();
+    let endpoint = iroh_net::endpoint::Endpoint::builder()
+        .discovery(Box::new(discovery))
+        .bind()
+        .await?;
+    //
+    let mut cosigners = Vec::new();
+    for cosigner in args.cosigners.iter() {
+        cosigners.push(send_cosign_request_round1(&endpoint, &cosigner, &args.key).await?);
+    }
+    let mut commitments_map = BTreeMap::new();
+    for (_, _, identifier, commitments) in cosigners.iter() {
+        commitments_map.insert(*identifier, commitments.clone());
+    }
+    let local_identifier = *key_package.identifier();
+    commitments_map.insert(local_identifier, commitments);
+    let signing_package = frost::SigningPackage::new(commitments_map, args.message.as_bytes());
+    let signing_package_bytes = signing_package.serialize()?;
+    let signing_package_bytes_len = signing_package_bytes.len() as u32;
+    let mut signature_shares = BTreeMap::new();
+    tracing::info!("Creating local signature share");
+    let local_signature_share = frost::round2::sign(&signing_package, &nonce, &key_package)?;
+    signature_shares.insert(local_identifier, local_signature_share);
+    for (mut send, mut recv, identifier, _) in cosigners {
+        send.write_all(&signing_package_bytes_len.to_be_bytes())
+            .await?;
+        send.write_all(&signing_package_bytes).await?;
+        let mut signature_share_bytes = [0u8; 32];
+        recv.read_exact(&mut signature_share_bytes).await?;
+        let signature_share = frost::round2::SignatureShare::deserialize(signature_share_bytes)?;
+        signature_shares.insert(identifier, signature_share);
+    }
+    tracing::info!("got {} signature shares", signature_shares.len());
+    let signature = frost::aggregate(&signing_package, &signature_shares, &public_key_package)?;
+    let bytes = signature.serialize();
+    println!("{}", hex::encode(&bytes));
+    let iroh_signature: iroh_net::key::Signature = bytes.into();
+    if let Err(cause) = key.verify(args.message.as_bytes(), &iroh_signature) {
+        tracing::error!("Verification failed: {:?}", cause);
+    }
     Ok(())
 }
 
 async fn cosign_daemon(args: CosignArgs) -> anyhow::Result<()> {
     let data_path = args.data_path.unwrap_or_else(|| PathBuf::from("."));
+    let secret_key = SecretKey::generate();
+    let discovery = PkarrPublisher::n0_dns(secret_key.clone());
     let mut keys = Vec::new();
     for entry in std::fs::read_dir(&data_path)? {
         let entry = entry?;
@@ -209,7 +327,9 @@ async fn cosign_daemon(args: CosignArgs) -> anyhow::Result<()> {
         }
     }
     let endpoint = iroh_net::endpoint::Endpoint::builder()
-        .alpns(vec![b"FROST_COSIGN".to_vec()])
+        .alpns(vec![COSIGN_ALPN.to_vec()])
+        .secret_key(secret_key)
+        .discovery(Box::new(discovery))
         .bind()
         .await?;
     let addr = endpoint.node_addr().await?;
@@ -231,8 +351,9 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     match args.cmd {
         Command::Split(args) => split(args)?,
-        Command::Sign(args) => sign(args)?,
+        Command::SignLocal(args) => sign_local(args)?,
         Command::Cosign(args) => cosign_daemon(args).await?,
+        Command::Sign(args) => sign(args).await?,
     }
     Ok(())
 }
